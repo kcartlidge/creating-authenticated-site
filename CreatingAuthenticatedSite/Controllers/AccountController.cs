c71a7ad1101f1872a4e584ccf93ac93263b1791e
K Cartlidge <code@kcartlidge.com>
2020-03-08 16:49:46 +0000

    Add the ability to login

---
+++ CreatingAuthenticatedSite/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace CreatingAuthenticatedSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Login()
+        {
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, "Example"));
+            identity.AddClaim(new Claim("ID", "1"));
+            identity.AddClaim(new Claim("IsAdmin", "Y"));
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+            return RedirectToAction("Dashboard", "Home");
+        }
+    }
+}
